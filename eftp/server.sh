6b6283030852967b656231353066cf1156693f55
Eric Sarabia <eric.sarabia@enti.cat>
2023-11-21 11:18:26 +0100

    Client: Obtenemos la IP

--- eftp/server.sh
+++ eftp/server.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 CLIENT="10.0.65.68"
-PORT="3333"
 echo "Servidor de EFTP"
 
 echo "(0) Listen"
 
-DATA=`nc -l -p $PORT -w 0`
+DATA=`nc -l -p 3333 -w 0`
 
 echo $DATA
 
@@ -16,16 +15,16 @@ if [ "$DATA" != "EFTP 1.0" ]
 then
 	echo "ERROR 1: BAD HEADER"
 	sleep 1
-	echo "KO_HEADER" | nc $CLIENT $PORT
+	echo "KO_HEADER" | nc $CLIENT 3333
 	exit 1
 fi
 echo "OK_HEADER"
 sleep 1
-echo "OK_HEADER" | nc $CLIENT $PORT
+echo "OK_HEADER" | nc $CLIENT 3333
 
 echo "(4) Listen"
 
-DATA=`nc -l -p $PORT -w 0`
+DATA=`nc -l -p 3333 -w 0`
 
 echo "(7) Test & Send"
 
@@ -33,12 +32,12 @@ if [ "$DATA" != "BOOOM" ]
 then 
 	echo "ERROR 2 : BAD HANDSHAKE"
 	sleep 1
-	echo "KO_HANDSHAKE" | nc $CLIENT $PORT
+	echo "KO_HANDSHAKE" | nc $CLIENT 3333
 	exit 2
 fi
 echo "OK_HANDSHAKE"
 sleep 1
-echo "OK_HANDSHAKE" | nc $CLIENT $PORT
+echo "OK_HANDSHAKE" | nc $CLIENT 3333
 
 echo "(8) Listen"
-DATA=`nc -l -p $PORT -w 0`
+DATA=`nc -l -p 3333 -w 0`
